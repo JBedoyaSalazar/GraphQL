4311fa1a5d660c4f94d6295a0fc6cfe9840c7431
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-07 14:34:54 -0500

    chore: update package.json to add development script, repository info, and dependencies

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 "use strict";
 
 const { graphql, buildSchema } = require("graphql");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+
 
 const schema = buildSchema(`
     type Query {
         hello: String
+        saludo: String
     }
 `);
 
 const root = {
   hello: () => "Hello GRAPHQL!",
+  saludo: () => "Hey There! This is a greeting from GraphQL!"
 };
 
-graphql({
-  schema,
-  source: '{ hello }',
-  rootValue: root
-}).then((result) => {
-  console.log(result);
+app.use("/api", graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/api`);
 });
