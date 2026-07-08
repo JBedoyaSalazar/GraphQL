52c65ccc6f1c266a34ce785725a399407129a5b6
Jefred Bedoya <jefred03salazar@gmail.com>
2026-07-07 23:33:22 -0500

    feat: integrate MongoDB connection and update resolvers for dynamic data fetching

---
+++ config/config.js
@@ -0,0 +1,8 @@
+export const config = {
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_NAME: process.env.DB_NAME,
+    MONGO_URI: process.env.MONGO_URI,
+};
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 "use strict";
-
+import dotenv from "dotenv/config";
 import { makeExecutableSchema } from "graphql-tools";
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
 import { readFileSync } from "fs";
 import path from "path";
 import root from "./lib/resolvers.js";
+import { config } from "./config/config.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
